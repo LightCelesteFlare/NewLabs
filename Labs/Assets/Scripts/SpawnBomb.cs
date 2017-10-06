bdd9b3536107d72aa0f2edd3a092ad06f26ed674
Brandon <Brandonkool16@gmail.com>
2017-10-06 02:57:14 -0400

    [Add]

---
+++ Labs/Assets/Scripts/SpawnBomb.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBomb : MonoBehaviour {
+
+    public GameObject bomb;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            print(Input.mousePosition);
+            Vector3 p = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f));
+
+            Instantiate(bomb, new Vector3(p.x, p.y, 0.0f), Quaternion.identity);
+
+        }
+    }
+}
