6c5644224da16f013874b54c6106280396acf404
Brandon <Brandonkool16@gmail.com>
2017-10-06 02:57:26 -0400

    [Mod]

--- Labs/Assets/Scripts/Explosion.cs
+++ Labs/Assets/Scripts/Explosion.cs
@@ -3,36 +3,69 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Explosion : MonoBehaviour {
-   
+
+    public GameObject explosion;
     public GameObject bomb;
-    public float ExposionRadius = 5.0f;
-    public float Exposionforce = 10.0f;
-    public float upforce = 0f;
-	// Use this for initialization
-	void Awake() {
-		
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-        if (bomb == enabled)
-        {
-            Invoke("Detonate", 5);
-        }
-	}
 
-    void Detonate()
+    public float explosionRate = 1.0f;
+    public float explosionforce = 25.0f;
+    public float explosion_max_size = 10f;
+    public float explodeDelay = 1.9f;
+    public float currentRadius = 0f;
+    
+    CircleCollider2D explosionRadius;
+
+
+    bool exploded = false;
+    // Use this for initialization
+    void Awake() {
+       explosionRadius  = gameObject.GetComponent<CircleCollider2D>();
+    }
+
+    void Update()
     {
-        Vector3 explosionPosition = bomb.transform.position;
-        Collider[] colliders = Physics.OverlapSphere(explosionPosition, ExposionRadius);
-        foreach (Collider hit in colliders)
+        explodeDelay -= Time.deltaTime;
+        if(explodeDelay < 0)
         {
-            Rigidbody rb = hit.GetComponent<Rigidbody>();
-            if (rb != null)
-            {
-                rb.AddExplosionForce(Exposionforce, explosionPosition, ExposionRadius, upforce, ForceMode.Impulse);
-            }
-
+            exploded = true;
         }
     }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 explosionPosition = bomb.transform.position;
+        if (explodeDelay <= 0)
+        {
+            if (currentRadius < explosion_max_size)
+            {
+                currentRadius += explosionRate;
+            }
+            else
+            {
+                Instantiate(explosion, explosionPosition, Quaternion.identity);
+                Object.Destroy(gameObject);
+            }
+
+            explosionRadius.radius = currentRadius;
+        }
+    }
+    
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Vector3 explosionPosition = bomb.transform.position;
+        Vector3 target = other.gameObject.transform.position;
+        
+
+        if (exploded == true)
+        {
+            if(other.gameObject.GetComponent<Rigidbody2D>() != null)
+            {
+                Vector3 direction = explosionforce * (target - explosionPosition);
+
+                other.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(direction.x / 2f, direction.y * 10f));
+            }
+        }
+    }
+
 }
