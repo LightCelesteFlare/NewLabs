ce86447451efab92aea297ace6d467f5c94ac4b4
Brandon <Brandonkool16@gmail.com>
2017-10-06 00:34:33 -0400

    [Mod]

--- Labs/Assets/Scripts/Explosion.cs
+++ Labs/Assets/Scripts/Explosion.cs
@@ -3,14 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Explosion : MonoBehaviour {
-
+   
+    public GameObject bomb;
+    public float ExposionRadius = 5.0f;
+    public float Exposionforce = 10.0f;
+    public float upforce = 0f;
 	// Use this for initialization
-	void Start () {
+	void Awake() {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void FixedUpdate () {
+        if (bomb == enabled)
+        {
+            Invoke("Detonate", 5);
+        }
 	}
+
+    void Detonate()
+    {
+        Vector3 explosionPosition = bomb.transform.position;
+        Collider[] colliders = Physics.OverlapSphere(explosionPosition, ExposionRadius);
+        foreach (Collider hit in colliders)
+        {
+            Rigidbody rb = hit.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.AddExplosionForce(Exposionforce, explosionPosition, ExposionRadius, upforce, ForceMode.Impulse);
+            }
+
+        }
+    }
 }
