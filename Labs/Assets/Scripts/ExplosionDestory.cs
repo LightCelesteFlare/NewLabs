b7adf06b09b2e3b87fb947b6528b3a3091d11341
Brandon <Brandonkool16@gmail.com>
2017-11-23 12:54:57 -0500

    [Mod]

--- Labs/Assets/Scripts/ExplosionDestory.cs
+++ Labs/Assets/Scripts/ExplosionDestory.cs
@@ -6,6 +6,6 @@ public class ExplosionDestory : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        Destroy(gameObject, 1.1f);
+        Destroy(gameObject, 1.05f);
 	}
 }
